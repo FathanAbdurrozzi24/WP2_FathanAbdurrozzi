34070282180ce030b13d881d6a736d088245e913
FathanAbdurrozzi24 <fthnabd24@gmail.com>
2024-05-20 15:58:28 +0700

    menambahkan Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Fathan Abdurrozzi
+        Saya tingga di Putussibau, Kapuas Hulu
+        Olahraga yang saya sukai adalah Sepak Bola";
+    }
+}
